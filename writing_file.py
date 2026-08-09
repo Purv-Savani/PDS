36a38e99358461924bc162da95381cd6c054994a
Purv-Savani <purvsavani@gmail.com>
2026-08-09 17:43:59 +0530

    Add file writing functionality in writing_file.py

---
+++ writing_file.py
@@ -0,0 +1,9 @@
+file = open("output.txt", "w")
+
+file.write("Python for Data Science\n")
+file.write("Learning file handling in Python\n")
+file.write("Writing data into a file")
+
+file.close()
+
+print("Data written successfully.")
